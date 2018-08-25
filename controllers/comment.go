ba8563c50be7f69f2db19e79d062ff303181e66f
FelixMH <felixmtzh.fm@gmail.com>
2018-08-25 04:42:17 -0500

    controllers/comment.go => new
    Añadida la funcion de crear comentario sin ID de usuario mediante el token.

--- controllers/comment.go
+++ controllers/comment.go
@@ -16,7 +16,10 @@ import (
 
 func CreateComment(w http.ResponseWriter, r *http.Request) {
 	comment := models.Comment{}
+	user := models.User{}
 	m := models.Message{}
+
+	user, _ = r.Context().Value("user").(models.User)
 
 	err := json.NewDecoder(r.Body).Decode(&comment)
 	if err != nil {
@@ -25,6 +28,8 @@ func CreateComment(w http.ResponseWriter, r *http.Request) {
 		commons.DisplayMessage(w, m)
 		return
 	}
+
+	comment.UserID = user.ID
 
 	db := configuration.GetConnection()
 	defer db.Close()
@@ -48,9 +53,9 @@ func CommentGetAll(w http.ResponseWriter, r *http.Request) {
 	comments := []models.Comment{}
 	m := models.Message{}
 	user := models.User{}
-	// vote := models.Vote{}
+	vote := models.Vote{}
 
-	r.Context().Value(&user)
+	user, _ = r.Context().Value("user").(models.User)
 	// Todo lo que llega despues del ? en la api.
 	vars := r.URL.Query()
 
@@ -80,6 +85,20 @@ func CommentGetAll(w http.ResponseWriter, r *http.Request) {
 		db.Model(&comments[i]).Related(&comments[i].User)
 		comments[i].User[0].Password = ""
 		comments[i].Children = commentGetChildren(comments[i].ID)
+
+		// Se busca el voto del usuario en sesión.
+		vote.CommentID = comments[i].ID
+		vote.UserID = user.ID
+		count := db.Where(&vote).Find(&vote).RowsAffected
+		if count > 0 {
+			if vote.Value {
+				comments[i].HasVote = 1
+			} else {
+				comments[i].HasVote = -1
+
+			}
+		}
+
 	}
 
 	j, err := json.Marshal(comments)
