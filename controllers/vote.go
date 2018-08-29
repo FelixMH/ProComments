ca4ea6275920bd9df23b784fde5f612d04170781
FelixMH <felixmtzh.fm@gmail.com>
2018-08-29 01:10:32 -0500

    Nuevo => vote.go
    Añadida la función de añadir voto a los comentarios, mediante la api.

---
+++ controllers/vote.go
@@ -0,0 +1,90 @@
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/FelixMH/ProComments/commons"
+	"github.com/FelixMH/ProComments/configuration"
+	"github.com/FelixMH/ProComments/models"
+)
+
+// VoteRegister: Controlador para registrar un voto.
+func VoteRegister(w http.ResponseWriter, r *http.Request) {
+	vote := models.Vote{}
+	user := models.User{}
+	currentVote := models.Vote{}
+	m := models.Message{}
+
+	user, _ = r.Context().Value("user").(models.User)
+	err := json.NewDecoder(r.Body).Decode(&vote)
+	if err != nil {
+		m.Message = fmt.Sprintf("Error al leer el usuario a registrar: %s", err)
+		m.Code = http.StatusBadRequest
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	vote.UserID = user.ID
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	db.Where("comment_id = ? and user_id = ?", vote.CommentID, vote.UserID).First(&currentVote)
+
+	// si no existe voto
+	if currentVote.ID == 0 {
+		db.Create(&vote)
+		err := updateCommentVotes(vote.CommentID, vote.Value)
+		if err != nil {
+			m.Message = err.Error()
+			m.Code = http.StatusBadRequest
+			commons.DisplayMessage(w, m)
+			return
+		}
+		m.Message = "Voto registrado"
+		m.Code = http.StatusCreated
+		commons.DisplayMessage(w, m)
+		return
+	} else if currentVote.Value != vote.Value {
+		currentVote.Value = vote.Value
+		db.Save(&currentVote)
+		err := updateCommentVotes(vote.CommentID, vote.Value)
+		if err != nil {
+			m.Message = err.Error()
+			m.Code = http.StatusBadRequest
+			commons.DisplayMessage(w, m)
+			return
+		}
+		m.Message = "Se actualizaron los Votos"
+		m.Code = http.StatusOK
+		commons.DisplayMessage(w, m)
+		return
+	}
+	m.Message = "Este voto ya está registrado."
+	m.Code = http.StatusBadRequest
+	commons.DisplayMessage(w, m)
+
+}
+
+func updateCommentVotes(commentID uint, vote bool) (err error) {
+	comment := models.Comment{}
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	rows := db.First(&comment, commentID).RowsAffected
+	if rows > 0 {
+		if vote {
+			comment.Votes++
+		} else {
+			comment.Votes--
+		}
+		db.Save(&comment)
+	} else {
+		err = errors.New("No se encontró un registro de comentario para asignarle un voto")
+	}
+	return
+}
